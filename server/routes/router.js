00be0d8cd522f9999703085782f32bcfcffafa4f
Steve Akinyemi <appcypher@outlook.com>
2017-08-07 08:13:24 +0100

    Implement api versioning

--- server/routes/router.js
+++ server/routes/router.js
@@ -8,27 +8,27 @@ router
   .get(controller);
 
 router
-  .route('/api/user/signup')
+  .route('/api/v1/user/signup')
   .get(controller);
 
 router
-  .route('/api/users/signin')
+  .route('/api/v1/users/signin')
   .get(controller);
 
 router
-  .route('/api/users/')
+  .route('/api/v1/users/')
   .get(controller);
 
 router
-  .route('/api/books')
+  .route('/api/v1/books')
   .get(controller);
 
 router
-  .route('/acontroller.getpi/books/:bookId')
+  .route('/api/v1/books/:bookId')
   .get(controller);
 
 router
-  .route('/api/users/:userId/')
+  .route('/api/v1/users/:userId/')
   .get(controller);
 
 
