83d49d0fe0966c8015aa76cf47ed11c1ca2b2fb4
Steve Akinyemi <appcypher@outlook.com>
2017-08-07 15:11:35 +0100

    Revert "Heroku deploy"

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # HelloBooks
 An elegant material design app that implements a simple digital library for keeping, borrowing and managing all kinds of literature.  
-
-
-# HELLO 
--- dist/controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = controller;
-function controller(req, res) {
-  res.send('Hi there!');
-}
