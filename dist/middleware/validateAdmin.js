9687be8c25a785db32a1c0b3520faa4edc4732a2
Steve Akinyemi <appcypher@outlook.com>
2017-08-09 16:25:51 +0100

    Update to heroku

--- dist/middleware/validateAdmin.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _bcrypt = require('bcrypt');
-
-var _bcrypt2 = _interopRequireDefault(_bcrypt);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
